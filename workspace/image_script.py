54eee4237a9af78228feecafbf2137c876a2a738
tasuku <tasuku@gmail.com>
2017-01-12 20:40:02 +0900

    image_script.py

--- workspace/image_script.py
+++ workspace/image_script.py
@@ -6,9 +6,14 @@ configpath = os.path.expanduser('~')
 file_dir = '/workspace/project/image-processor/workspace/images'
 
 if __name__=="__main__":
-    imgs = glob.glob('{}/*.png'.format(configpath+file_dir))
+    imgs = glob.glob('{}/*.png'.format(os.join(configpath,file_dir)))
   
     for i,img in enumerate(imgs):
-        call(["./BlurImage", img, "{}.png".format(i)])
+        try:
+            call(["./BlurImage", img, "{}.png".format(i)])
+        except IOError:
+            print("Call process failed.")
+
+            
 
 
