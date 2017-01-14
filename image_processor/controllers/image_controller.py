08e566854648606900bf9125baf80c4f7632e649
tasuku <tasuku@gmail.com>
2017-01-14 12:48:00 +0900

    updated

--- image_processor/controllers/image_controller.py
+++ image_processor/controllers/image_controller.py
@@ -1,11 +1,18 @@
 #!/usr/local/bin/python3
+from werkzeug.utils import secure_filename 
 import models as dbH
- 
-def add_image() -> int:
-    """Adds image id to database, and returns
-    image id. 
+
+IMG_BANK_PATH = '/workspace/project/image-processor/image-processor/image_bank/'
+
+def add_image(f=None) -> int:
+    """Adds image id to database, stores image to path
+    and returns image id. 
     """    
     id = dbH.insert_image() 
+    if f:
+        f_name = secure_filename(f.filename)
+        f.save(os.path.join(IMG_BANK_PATH, f_name))
+
     return str(id)
 
 def delete_images(imageId, apiKey = None) -> str:
