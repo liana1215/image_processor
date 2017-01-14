973a2bb7ab9fef301b9365b05bddc6a913f42585
tasuku <tasuku@gmail.com>
2017-01-14 11:21:22 +0900

    updated

--- workspace/image_script.py
+++ workspace/image_script.py
@@ -3,11 +3,10 @@ import os
 from subprocess import call
 
 configpath = os.path.expanduser('~')
-file_dir = '/workspace/project/image-processor/workspace/images'
+file_dir = "workspace/project/image-processor/workspace/images"
 
 if __name__=="__main__":
     imgs = glob.glob("{}/*.png".format(os.path.join(configpath,file_dir)))
-  
     for i,img in enumerate(imgs):
         try:
             call(["./BlurImage", img, "{}.png".format(i)])
