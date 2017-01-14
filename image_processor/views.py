d8d35986aa60483fdf381e08165e30a4e9a14818
tasuku <tasuku@gmail.com>
2017-01-14 12:49:32 +0900

    updated

--- image_processor/views.py
+++ image_processor/views.py
@@ -2,22 +2,23 @@
 
 import os
 
-from flask import Flask, Blueprint, g, request
-from werkzeug.utils import secure_filename
+from flask import Flask, Blueprint, g, request, flash
 import controllers.image_controller as imgc
 
 view = Blueprint('view', __name__)
-IMG_BANK_PATH = '/workspace/project/image-processor/image-processor/image_bank/'
+
 #http://flask.pocoo.org/docs/0.12/patterns/fileuploads/
 @view.route('/images', methods=['POST','GET'])
 def images():
     if request.method == 'POST':
-        ret_id = imgc.add_image()
-        #f = request.files["file"]
-        #if f:
-        #    f_name = secure_filename(f.filename)
-        #    f.save(os.path.join(IMG_BANK_PATH, f_name))
-            
-        #add image to dir with id
+        
+        if 'file' not in request.files:
+            print('No file')
+        else:
+            ret_id = imgc.add_image(request.files['file'])
+                
+
+        ret_id = imgc.add_image() # this will be removed, just for prototyping
         return ret_id
-       
+
+        
